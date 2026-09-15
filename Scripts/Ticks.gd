d7bb30c12eae979cb7bc7e64b54b7b61461a557c
yukithetupper <horatioesf@protonmail.com>
2026-09-15 18:13:51 +0100

    time controls

--- Scripts/Ticks.gd
+++ Scripts/Ticks.gd
@@ -19,15 +19,19 @@ var tick_id = 0
 var tick_speed = 1
 var tick_timer = 0
 
+var game_paused = false
+var speed_multiplier = 1.0
+
 func _ready():
 	tick.connect(proc_tick)
 
 func _process(delta):
-	tick_timer += delta
-	if tick_timer > tick_speed:
-		tick_timer -= tick_speed
-		tick_id += 1
-		emit_signal("tick")
+	if !game_paused:
+		tick_timer += delta * speed_multiplier
+		if tick_timer > tick_speed:
+			tick_timer -= tick_speed
+			tick_id += 1
+			emit_signal("tick")
 	
 func proc_tick():
 	tick_production()
@@ -45,3 +49,16 @@ func tick_consumption():
 			
 	resources["food"] -= net_food_eat
 	
+func get_date_string():
+	var days = floor(tick_id / 6.0)
+	
+	return Time.get_date_dict_from_unix_time(days * 86400)
+
+func toggle_pause() -> void:
+	game_paused = !game_paused
+
+func increment_speed() -> void:
+	speed_multiplier += 0.5
+	
+	if speed_multiplier > 4.0:
+		speed_multiplier = 1.0
