bc37bd2f595ce6d3088546377fdb985d386b614e
yukithetupper <horatioesf@protonmail.com>
2026-09-19 16:39:07 +0100

    trace gfx

--- Prefabs/Entities/civil.gd
+++ Prefabs/Entities/civil.gd
@@ -1,4 +1,8 @@
 extends Building
 
+func _ready():
+	super()
+	$Sprite2D.frame = randi()%3
+
 func _process(delta: float):
 	super(delta)
--- Scenes/world.gd
+++ Scenes/world.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var tile_size = Vector2(32, 32)
-var line_color = Color(1.0, 1.0, 1.0, 0.137)
+var tile_size = Vector2(93, 93)
+var line_color = Color(0.0, 1.0, 0.0, 0.259)
 
 func _ready():
 	get_tree().set_meta("world", self)
@@ -11,20 +11,22 @@ func _process(delta):
 	queue_redraw()
 	
 func _draw():
+	return
 	var screen_size = Vector2(1440, 1080)
 	
 	var number_of_horizontal_lines = floor(screen_size.y / tile_size.y) + 4
 	var number_of_vertical_lines = floor(screen_size.x / tile_size.x) + 4
 	
 	var camera_offset = get_node("../Camera2D").position
+	var thickness = 4.0
 	
 	for y in number_of_horizontal_lines:
 		var y_pos = tile_size.y * (y - number_of_horizontal_lines/2) + floor(camera_offset.y/tile_size.y) * tile_size.y
-		draw_line(Vector2(camera_offset.x - screen_size.x/2.0, y_pos), Vector2(camera_offset.x + screen_size.x/2.0, y_pos), line_color, 1.0)
+		draw_line(Vector2(camera_offset.x - screen_size.x/2.0, y_pos), Vector2(camera_offset.x + screen_size.x/2.0, y_pos), line_color, thickness)
 	
 	for x in number_of_vertical_lines:
 		var x_pos = tile_size.x * (x - number_of_vertical_lines/2) + floor(camera_offset.x/tile_size.x) * tile_size.x
-		draw_line(Vector2(x_pos, camera_offset.y - screen_size.y/2.0), Vector2(x_pos, camera_offset.y + screen_size.y/2.0), line_color, 1.0)
+		draw_line(Vector2(x_pos, camera_offset.y - screen_size.y/2.0), Vector2(x_pos, camera_offset.y + screen_size.y/2.0), line_color, thickness)
 	
 func generate_world() -> void:
 	var admin = load("res://Prefabs/Entities/admin.tscn").instantiate()
@@ -84,7 +86,7 @@ func generate_world() -> void:
 		$buildings.add_child(village)
 	
 	
-	for i in range(40):
+	for i in range(10):
 		var radius = 400
 		var civil = load("res://Prefabs/Entities/civil.tscn").instantiate()
 		var building_position = Vector2(720, 540) + Vector2(radius * randf(), 0).rotated(2 * PI * randf())
